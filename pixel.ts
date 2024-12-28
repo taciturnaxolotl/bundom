a4f9a0221b563087b63fc62c01dd22d5e0dc6355
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-12-28 11:33:13 -0500

    bug: add delay

--- pixel.ts
+++ pixel.ts
@@ -1,4 +1,10 @@
 import { PNG } from "pngjs";
+import Bottleneck from "bottleneck";
+
+const neck = new Bottleneck({
+  maxConcurrent: 1,
+  minTime: 3000,
+});
 
 async function setPixelColor(
   x: number,
@@ -6,15 +12,18 @@ async function setPixelColor(
   r: number,
   g: number,
   b: number,
-): Promise<{ success: boolean }> {
+) {
   return await fetch("https://place.danieldb.uk/set_pixel_color", {
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({ x, y, r, g, b }),
     method: "POST",
-  }).then((res) => res.json());
+  });
 }
 
-// read 64x64.png using bun file
-const file = await Bun.file("64x64.png").arrayBuffer();
+// // read 64x64.png using bun file
+const file = await Bun.file("corgi.png").arrayBuffer();
 const png = PNG.sync.read(Buffer.from(file));
 
 console.log({ width: png.width, height: png.height });
@@ -26,7 +35,6 @@ for (let y = 0; y < png.height; y++) {
     const r = png.data[idx];
     const g = png.data[idx + 1];
     const b = png.data[idx + 2];
-    console.log({ x, y, r, g, b });
-    await setPixelColor(x, y, r, g, b);
+    neck.schedule(setPixelColor, x, y, r, g, b);
   }
 }
